13d1bad31b6d3fe51c37752d4b7353cb832d92ff
andreluizalves <andreluiz49.al@gmail.com>
2020-04-01 21:46:36 -0300

    add app'

--- limasburger/settings.py
+++ limasburger/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['limasburger.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
