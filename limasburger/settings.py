b0fcba5710b3abbca4ba2b5da8b5d9bace6e916c
andreluizalves <andreluiz49.al@gmail.com>
2020-04-05 12:12:19 -0300

    imagens

--- limasburger/settings.py
+++ limasburger/settings.py
@@ -117,9 +117,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'statics')  
-]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "{}/media".format(BASE_DIR)
