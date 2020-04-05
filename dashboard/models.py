ce4582d13eaefa89d8f5c3df8fa22dff02d429b2
andreluizalves <andreluiz49.al@gmail.com>
2020-04-05 12:23:35 -0300

    imagens

--- dashboard/models.py
+++ dashboard/models.py
@@ -20,7 +20,7 @@ class Produto(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=100, choices=status_choices)
     ingredientes = models.ManyToManyField(to=Ingrediente)
-    imagem = models.FileField(upload_to="%Y/%m/%d", null=True, )
+    imagem = models.FileField(upload_to="", null=True, )
     def __str__(self):
         return self.nome
 
