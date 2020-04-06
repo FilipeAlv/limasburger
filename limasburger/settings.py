d177f9425cf2a5b3c46ef127fe5916cc2b8f7feb
andreluizalves <andreluiz49.al@gmail.com>
2020-04-06 18:35:03 -0300

    aws

--- limasburger/settings.py
+++ limasburger/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['limasburger.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['limasburger.herokuapp.com', 'localhost', 'ec2-18-229-29-129.sa-east-1.compute.amazonaws.com']
 
 
 # Application definition
