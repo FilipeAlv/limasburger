d66a7910756f640b3976643599107e6c387af15c
FelipeDAlencar <fdouglas7@gmail.com>
2020-05-13 18:25:51 -0300

    teste pedido 6

--- dashboard/views.py
+++ dashboard/views.py
@@ -25,7 +25,7 @@ def listarProdutosPorNome(request, nome):
     return HttpResponse(serializers.serialize("json", produtos))
 
 def listarPedidoPorUser(request, id):
-    cliente = models.Usuario.objects.filter(id=id)
+    cliente = models.Usuario.objects.all()
     print("ID")
     print(id)
     print("Cliente")
