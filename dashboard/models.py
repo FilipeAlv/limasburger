73a671e02621218bd9773f0e40fe671b11867799
FelipeDAlencar <fdouglas7@gmail.com>
2020-05-13 18:30:43 -0300

    teste pedido 8

--- dashboard/models.py
+++ dashboard/models.py
@@ -51,8 +51,7 @@ class Usuario(models.Model):
     enderecos = models.ManyToManyField(to=Endereco)
     contato = models.CharField(max_length=20, blank=False)
     status = models.CharField(max_length=30, blank=False)
-    def __str__(self):
-        return self.nome + " - " + self.id
+
 
 class Pedido(models.Model):
     CHOICES_STATUS = [
