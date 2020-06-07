6e09d926c5f96eefd01cde7f486d08fe05a37c02
FelipeDAlencar <fdouglas7@gmail.com>
2020-06-07 15:11:00 -0300

    Promocao

--- dashboard/models.py
+++ dashboard/models.py
@@ -28,8 +28,8 @@ class Produto(models.Model):
     ingredientes = models.ManyToManyField(to=Ingrediente)
     imagem = models.FileField(upload_to="", null=True, )
     promocao = models.ForeignKey(
-        to=Promocao, on_delete=models.CASCADE, blank=True, null = True)
-        
+        to=Promocao, on_delete=models.CASCADE, blank=True, null = False)
+
     def __str__(self):
         return self.nome
 
