52f4039ae80ee1c5701caa54f005495c42042300
FelipeDAlencar <fdouglas7@gmail.com>
2020-06-10 23:27:01 -0300

    jet - url

--- limasburger/urls.py
+++ limasburger/urls.py
@@ -20,8 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('jet/', include('jet.urls', 'jet')),
-    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('', admin.site.urls),
     path('buscar/produtos/<int:init>-<int:fim>', views.listarProdutosCatalogo),
     path('buscar/produtos/<str:nome>-<int:ignore>', views.listarProdutosFilter),
