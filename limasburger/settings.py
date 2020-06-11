e2c705c3f52dfece3468f560f4e42535d18d195d
FelipeDAlencar <fdouglas7@gmail.com>
2020-06-10 23:04:22 -0300

    jet - url

--- limasburger/settings.py
+++ limasburger/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-  	'jet_django',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
